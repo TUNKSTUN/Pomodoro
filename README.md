0ded44da3c8a56405dc488bdacda09af77bc8391
TUNKSTUN <johnwick4learning@gmail.com>
2023-03-20 08:05:35 +0530

    Pomodoro App Build

---
+++ README.md
@@ -0,0 +1,34 @@
+
+# 🍅 Pomodoro App
+
+This is a pomodoro app built with Electron that allows you to adjust the time and date, switch between light and dark mode themes, and keep track of your work sessions.
+
+## How to Run the Application 🚀
+
+1.  Clone this repository to your local machine.
+2.  Install the necessary dependencies by running `npm install` in your terminal.
+3.  Start the app by running `npm start` in your terminal.
+
+## Features 🌟
+
+-   Adjust the time for work and break sessions to fit your needs.
+-   Keep track of the current time and date.`
+-   Switch between light and dark mode themes.
+-   Start, pause, and reset the timer for your work sessions.
+-   Get notifications when your work or break session is complete.
+
+## Technologies Used 🛠️
+
+-   Electron
+-   JavaScript
+-   HTML
+-   CSS
+
+## Acknowledgements 🙏
+
+This app was inspired by the Pomodoro Technique, a time management method developed by Francesco Cirillo.
+<hr>
+
+### Give a ⭐️ if You Find This Repo Interesting ❤️
+
+Happy Coding! 💻👨‍💻👩‍💻
---
+++ src/index.js
@@ -0,0 +1,62 @@
+const { app, BrowserWindow, shell } = require('electron');
+const path = require('path');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+// eslint-disable-next-line global-require
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 500,
+    height: 500,
+    resizable: false,
+    autoHideMenuBar: true,
+    transparent: true,
+    thickFrame: true,
+    movable: true,
+    hasShadow: false,
+    webPreferences: {
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js'),
+      icon: path.join(__dirname, 'assets/abc.ico') 
+    },
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.webContents.on('new-window', function(e, url) {
+    e.preventDefault();
+    require('electron').shell.openExternal(url);
+  });
+
+  mainWindow.on('closed', function () {
+  })
+};
+
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
